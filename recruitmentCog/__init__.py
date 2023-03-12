1ea5f9a6a3f671f370b77cbb5ecf754e357225c3
unknown <jacobdamkjaer@gmail.com>
2023-03-12 18:31:06 +0100

    Removed await

--- recruitmentCog/__init__.py
+++ recruitmentCog/__init__.py
@@ -1,5 +1,6 @@
 from redbot.core.bot import Red
 from .recruitment import Recruitment
 
-async def setup(bot: Red) -> None:
-    await bot.add_cog(Recruitment(bot))
+
+def setup(bot: Red):
+    bot.add_cog(Recruitment(bot))
