ce43a54f5ca7c44ba32e77ef6954cc94c9889360
unknown <jacobdamkjaer@gmail.com>
2023-03-14 17:45:58 +0100

    Argh, more debugging

--- reginaldCog/reginald.py
+++ reginaldCog/reginald.py
@@ -1,14 +1,16 @@
 import openai
-from redbot.core import commands
+from redbot.core import Config, commands
 from threading import Lock
 from ratelimit import rate_limited
 
 class ReginaldCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.api_key = self.bot.config.get("openai_api_key")
-        openai.api_key = self.api_key
-        self.model = self.bot.config.get("openai_model")
+        self.config = Config.get_conf(self, identifier=1234567890)
+        self.config.register_global(
+            openai_api_key="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",
+            openai_model="text-davinci-002"
+        )
         self.lock = Lock()
 
     @rate_limited(1, 10) # Limits command execution to 1 per 10 seconds
@@ -20,6 +22,9 @@ class ReginaldCog(commands.Cog):
             prompt = "Hey,"
         async with ctx.typing(), self.lock:
             try:
+                self.api_key = await self.config.openai_api_key()
+                self.model = await self.config.openai_model()
+                openai.api_key = self.api_key
                 max_tokens = min(len(prompt) * 2, 2048)
                 response = openai.Completion.create(
                     model=self.model,
@@ -35,7 +40,5 @@ class ReginaldCog(commands.Cog):
                 print(f"OpenAI API Error: {e}")
 
 def setup(bot):
-    bot.config.register("openai_api_key", default="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")
-    bot.config.register("openai_model", default="text-davinci-002")
     cog = ReginaldCog(bot)
     bot.add_cog(cog)
