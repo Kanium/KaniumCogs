b915280a5496a1a50e85f8f1f8ab30637e577322
unknown <jacobdamkjaer@gmail.com>
2023-06-01 22:06:12 +0200

    Attempting to fix recruitmentCog to support Red bot 3.5

--- recruitmentCog/__init__.py
+++ recruitmentCog/__init__.py
@@ -2,6 +2,6 @@ from redbot.core import commands
 from .recruitment import Recruitment
 
 
-def setup(bot: commands.Bot) -> None:
+async def setup(bot: commands.Bot) -> None:
     cog = Recruitment(bot)
-    bot.add_cog(cog)
+    await bot.add_cog(cog)
