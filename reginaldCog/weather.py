cc4e7ae52830b93c10c10439748161879299f784
AllfatherHatt <jacobdamkjaer@gmail.com>
2025-03-16 13:22:48 +0100

    No more naughty, all is fine

--- reginaldCog/weather.py
+++ reginaldCog/weather.py
@@ -24,8 +24,8 @@ def get_weather_forecast(location: str, days: int = 14, dt: str = '2025-03-24') 
 class Weather:
     def __init__(self, location: str):
         self.__location = location
-        self.api_key = "f6e5cb73cae242deb2b63903250803" 
-        
+        self.api_key = environ.get('WEATHER_API_KEY')
+
     @property
     def location(self) -> str:
         return self.__location
